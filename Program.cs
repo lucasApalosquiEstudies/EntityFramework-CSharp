39043d2abc0c6c6177ea57ce2b1e34e04302756b
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-24 19:03:07 -0300

    :sparkles: feat: Create Related data

--- Program.cs
+++ Program.cs
@@ -13,8 +13,36 @@ namespace BlogEntity
             using (var context = new DataContext())
             {
 
+                var user = new User
+                {
+                    Name = "Luizito",
+                    Email = "luizito@email.com",
+                    Slug = "luizi-to",
+                    Bio = "L.U.I.Z.I.T.O",
+                    Image = "https:",
+                    PasswordHash = "Hash",
+                };
 
+                var category = new Category
+                {
+                    Name = "Back-End",
+                    Slug = "backend",
+                };
 
+                var post = new Post
+                {
+                    Title = "Começando EF Core",
+                    Summary = "EF Core",
+                    Body = "EF Core dos crias",
+                    Slug = "ef-core",
+                    CreateDate = DateTime.Now,
+                    LastUpdateDate = DateTime.Now,
+                    Author = user,
+                    Category = category,
+                };
+
+                context.Posts.Add(post);
+                context.SaveChanges();
             }
         }
     }
