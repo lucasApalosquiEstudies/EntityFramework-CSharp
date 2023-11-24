b9177bda062a3bde2ed0e27376bc7d95bc5f277f
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-24 18:17:30 -0300

    :sparkles: feat: Mapping with foreign key

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlogEntity.Models
 {
+    [Table("User")]
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
