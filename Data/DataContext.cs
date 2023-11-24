40af038f74d1bde9abd605f7b5a4bf61a3a895a5
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-24 12:55:24 -0300

    :sparkles: feat: DataContext First Config

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogEntity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogEntity.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+}
