3c7d483174421a390fd47f2c008e2d7b721b01c6
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-24 12:38:40 -0300

    :sparkles: feat: PostTag Model

---
+++ Models/PostTag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogEntity.Models
+{
+    public class PostTag
+    {
+        public int PostId { get; set; }
+        public int TagId { get; set; }
+    }
+}
