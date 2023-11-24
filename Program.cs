5ac567cfdd93ce8379cde4c85701f8f64a291940
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-24 18:06:24 -0300

    :sparkles: feat: Mapping Category

--- Program.cs
+++ Program.cs
@@ -12,32 +12,6 @@ namespace BlogEntity
         {
             using (var context = new DataContext())
             {
-                //var tag = new Tag { Name = "Python" , Slug = "PyCharm"  };
-                //context.Tags.Add(tag);
-                //context.SaveChanges();
-
-                //var tag = context.Tags.FirstOrDefault(x => x.Id == 6);
-                //tag.Name = "ASP.NETCore";
-                //context.Tags.Update(tag);
-                //context.SaveChanges();
-
-                //var tag = context.Tags.FirstOrDefault(x => x.Id == 7);
-                //context.Tags.Remove(tag);
-                //context.SaveChanges();
-
-                var tags = context
-                            .Tags
-                            .AsNoTracking()
-                            .Where(x => x.Name.Contains("A"))
-                            .ToList();
-
-
-                foreach (var tag in tags)
-                {
-                    Console.WriteLine($"Tag Name: {tag.Name} Slug: {tag.Slug}");
-                }
-
-
 
 
 
