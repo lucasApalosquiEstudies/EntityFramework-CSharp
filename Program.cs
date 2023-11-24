83677092a0c98ffbdef1cf0f4f88488b25f481ef
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-24 14:02:09 -0300

    :sparkles: feat: Update Tag

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using BlogEntity.Data;
 using BlogEntity.Models;
 using System;
+using System.Linq;
 
 namespace BlogEntity
 {
@@ -10,8 +11,13 @@ namespace BlogEntity
         {
             using (var context = new DataContext())
             {
-                var tag = new Tag { Name = "ASP.NET" , Slug = "aspnet"  };
-                context.Tags.Add(tag);
+                //var tag = new Tag { Name = "Python" , Slug = "PyCharm"  };
+                //context.Tags.Add(tag);
+                //context.SaveChanges();
+
+                var tag = context.Tags.FirstOrDefault(x => x.Id == 6);
+                tag.Name = "ASP.NETCore";
+                context.Tags.Update(tag);
                 context.SaveChanges();
 
             }
