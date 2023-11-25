070a824292b0b125ee43ada8b99f7f926d85d36b
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-25 09:44:42 -0300

    :sparkles: feat: Read With Include

--- Program.cs
+++ Program.cs
@@ -13,36 +13,47 @@ namespace BlogEntity
             using (var context = new DataContext())
             {
 
-                var user = new User
-                {
-                    Name = "Luizito",
-                    Email = "luizito@email.com",
-                    Slug = "luizi-to",
-                    Bio = "L.U.I.Z.I.T.O",
-                    Image = "https:",
-                    PasswordHash = "Hash",
-                };
+                //var user = new User
+                //{
+                //    Name = "Luizito",
+                //    Email = "luizito@email.com",
+                //    Slug = "luizi-to",
+                //    Bio = "L.U.I.Z.I.T.O",
+                //    Image = "https:",
+                //    PasswordHash = "Hash",
+                //};
 
-                var category = new Category
-                {
-                    Name = "Back-End",
-                    Slug = "backend",
-                };
+                //var category = new Category
+                //{
+                //    Name = "Back-End",
+                //    Slug = "backend",
+                //};
 
-                var post = new Post
-                {
-                    Title = "Começando EF Core",
-                    Summary = "EF Core",
-                    Body = "EF Core dos crias",
-                    Slug = "ef-core",
-                    CreateDate = DateTime.Now,
-                    LastUpdateDate = DateTime.Now,
-                    Author = user,
-                    Category = category,
-                };
+                //var post = new Post
+                //{
+                //    Title = "Começando EF Core",
+                //    Summary = "EF Core",
+                //    Body = "EF Core dos crias",
+                //    Slug = "ef-core",
+                //    CreateDate = DateTime.Now,
+                //    LastUpdateDate = DateTime.Now,
+                //    Author = user,
+                //    Category = category,
+                //};
 
-                context.Posts.Add(post);
-                context.SaveChanges();
+                //context.Posts.Add(post);
+                //context.SaveChanges();
+
+                var posts = context.Posts
+                    .AsNoTracking()
+                    .Include(x => x.Author)
+                    .OrderByDescending(x => x.LastUpdateDate)
+                    .ToList();
+
+                foreach (var post in posts)
+                {
+                    Console.WriteLine($"{post.Title} | Autor: {post.Author.Name}");
+                }
             }
         }
     }
