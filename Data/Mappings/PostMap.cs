e610936edc8403d78c016f9c6fb7c91274e9f44a
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-29 14:00:22 -0300

    :sparkles: feat(fluentBlog): One To Many and Many to Many*

--- Data/Mappings/PostMap.cs
+++ Data/Mappings/PostMap.cs
@@ -72,6 +72,22 @@ namespace BlogEntity.Data.Mappings
                 .WithMany(x => x.Posts)
                 .HasConstraintName("FK_Post_Category");
 
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_PostTag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                );
+
         }
     }
 }
