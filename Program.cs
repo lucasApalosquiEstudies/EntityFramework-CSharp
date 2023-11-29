b7c677649a390125e9caa5a41af352a6506e67f0
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-29 15:28:44 -0300

    :sparkles: feat(Migration): Ef install and first migration

--- Program.cs
+++ Program.cs
@@ -12,51 +12,7 @@ namespace BlogEntity
         {
             using (var context = new DataContext())
             {
-                //context.Users.Add(new User
-                //{
-                //    Name = "Paozinho",
-                //    Email = "Pao@email.com",
-                //    Bio = "Pao de sal",
-                //    Image = "https:",
-                //    PasswordHash = "hash123",
-                //    Slug = "paozinho"
-                //});
-
-                //var user = context.Users.First();
-
-
-                //var post = new Post
-                //{
-                //    Title = "Receita de pão",
-                //    Body = "Ovo, água e farinha",
-                //    Summary = "Receitinha",
-                //    Slug = "receita-pao",
-                //    Category = new Category
-                //    {
-                //        Name = "Receita",
-                //        Slug = "receita"
-                //    },
-                //    Author = user,
-
-                //};
-
                 
-
-                var Tag = new Tag
-                {
-                    Name = "Reiceitas",
-                    Slug = "receititasss"
-                };
-
-                var post = context.Posts
-                    .AsNoTracking()
-                    .FirstOrDefault(x => x.Id == 3);
-
-                //post.Tags.Add(Tag);
-                //context.Posts.Add(post);
-                context.Posts.Remove(post);
-
-                context.SaveChanges();
                
             }
         }
